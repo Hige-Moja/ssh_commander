b2d4001808ae32bfc9c309db10f559dbfadddbc1
Hige-Moja <13044551+Hige-Moja@users.noreply.github.com>
2018-02-21 13:53:03 +0900

    [fix] function improvement - The command can be specified as the first parameter. - The path of the server files can be specified as the secound parameter. - It divided for each function

--- ssh_exec_commander.sh
+++ ssh_exec_commander.sh
@@ -2,45 +2,57 @@
 #In advance you need copy the host public key to remote servers.
 
 function f_ssh_commander() {
-  CMD=${key}
-  grep -v -e '^\s*#' -e '^\s*$' ${SERVERLIST} | awk 1 | while read server
-  do
+  grep -v -e '^\s*#' -e '^\s*$' ${SERVERLIST} | awk 1 | while read server ; do
     echo "${server} / ${CMD}"
     ssh -n ${server} ${CMD}
     echo ''
   done
 }
 
-function f_firstmsg() {
+function f_set_command_msg() {
 echo " Please enter the command that you want to execute.  e.g. ls -la /service | grep pam"
 echo " If you want to exit, input 'quit'"
 echo ''
 }
 
+function f_set_command() {
+if [ -z "${CMD}" ]; then
+  f_set_command_msg
+  while read key ; do
+    case ${key} in
+      exit|q|qi|quit  ) exit ;;
+      *               ) [ ! -z "${key}" ] && CMD=${key} && break ;;
+    esac
+    f_set_command_msg
+  done
+fi
+}
+
 function f_set_serverlist() {
-echo " Please enter your server listfile."
-echo " If nothing, will use ./serverlist_for_ssh_commander.txt"
-while read filename ; do
-case ${filename} in
-"" )    SERVERLIST=./serverlist_for_ssh_commander.txt
-        break ;;
-*  )    [ -e ${filename} ] && SERVERLIST=${filename} && break
-        echo "Ther serverlist not found."
-        echo '' 
-        echo "Enter your serverlist file."
-        echo "If nothing, it will use ./serverlist_for_ssh_commander.txt" ;;
-esac
-done
-echo '' 
+if [ "${PARAM}" -eq "1" ]; then
+  SERVERLIST=./serverlist_for_ssh_commander.txt
+else
+  echo " Please enter your server listfile."
+  echo " If nothing, will use ./serverlist_for_ssh_commander.txt"
+  while read filename ; do
+    case ${filename} in
+    ""  ) SERVERLIST=./serverlist_for_ssh_commander.txt
+          break ;;
+    *   ) [ -e ${filename} ] && SERVERLIST=${filename} && break
+          echo "Ther serverlist not found."
+          echo ''
+          echo "Enter your serverlist file."
+          echo "If nothing, it will use ./serverlist_for_ssh_commander.txt" ;;
+    esac
+  done
+  echo ''
+fi
 }
 
 ## MAIN
-f_firstmsg
-while read key ; do
-  case ${key} in
-    exit|q|qi|quit ) break ;;
-         * )    f_set_serverlist
-                f_ssh_commander ;;
-  esac
-  f_firstmsg
-done
+PARAM=$#
+CMD=$1
+SERVERLIST=$2
+f_set_command
+f_set_serverlist
+f_ssh_commander
